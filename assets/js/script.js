1e66d0bf5ac15ef3d289f76473e2f43ff7e4fdc0
artolajon <artolajon2@gmail.com>
2022-11-09 22:26:02 +0100

    Optimize web design

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,31 @@
-function useForm(useIt){
-    let form = document.getElementById("form");
-    let social = document.getElementById("social");
+function showForm(display){
+    changeVisibility("form", display);
+    changeVisibility("social", !display);
+}
 
-    let useForm = document.getElementById("useForm");
-    let useSocial = document.getElementById("useSocial");
-    if (useIt){
-        social.classList.remove("active");
-        form.classList.add("active");
+function changeVisibility(elementType, display){
+    let div = document.getElementById(elementType);
+    let link = document.getElementById("use-"+elementType);
 
-        useForm.classList.remove("active");
-        useSocial.classList.add("active");
-        
+    if (display){
+        show(div);
+        hide(link);
     }else{
-        form.classList.remove("active");
-        social.classList.add("active");
-
-        useSocial.classList.remove("active");
-        useForm.classList.add("active");
+        hide(div);
+        show(link);
     }
+}
+
+function hide(element){
+    element.classList.add("hide");
+    setTimeout(()=>{
+        element.classList.remove("active");
+    }, 250);
+}
+
+function show(element){
+    setTimeout(()=>{
+        element.classList.add("active");
+        element.classList.remove("hide");
+    }, 250);
 }
